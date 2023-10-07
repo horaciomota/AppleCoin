1b44bac61d4a3f1dba76367b267769fa3fafe7a5
Horacio Mota <horaciomota@Horacios-MacBook-Pro.local>
2023-10-07 12:56:43 -0700

    Finished splashScreen

---
+++ AppleCoin/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  AppleCoin
+//
+//  Created by Horacio Mota on 07/10/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgb: UInt64 = 0
+
+        scanner.scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
--- AppleCoin/View/SplashScreen/SplashScreen.swift
+++ AppleCoin/View/SplashScreen/SplashScreen.swift
@@ -7,21 +7,6 @@
 
 import SwiftUI
 
-extension Color {
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        var rgb: UInt64 = 0
-
-        scanner.scanHexInt64(&rgb)
-
-        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
-        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
-        let blue = Double(rgb & 0x0000FF) / 255.0
-
-        self.init(red: red, green: green, blue: blue)
-    }
-}
-
 struct SplashScreen: View {
     var body: some View {
 
@@ -65,11 +50,11 @@ struct SplashScreen: View {
                     .resizable()
                     .frame(width: 365, height: 561)
                     .padding(.bottom, 100)
-                Image("Vector")
+                Image("estatua")
                     .resizable()
                     .frame(width: 365, height: 561)
                     .padding(.bottom, 100)
-                Image("estatua")
+                Image("Vector")
                     .resizable()
                     .frame(width: 365, height: 561)
                     .padding(.bottom, 100)
